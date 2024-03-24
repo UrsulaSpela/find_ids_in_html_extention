079ab20b0306a144eebb4cf2bd0be9cd519a2bd3
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2024-03-24 16:48:09 +0100

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const searchBtn = document.getElementById("searchBtn");
+  const input = document.getElementById("userInput");
+
+  function getUserInputValue() {
+    return input.value;
+  }
+
+  searchBtn.addEventListener("click", async () => {
+    const inputValue = getUserInputValue();
+    let [tab] = await chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true,
+    });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: checkExistingIds,
+      args: [inputValue], // Pass inputValue as an argument to the content script
+    });
+  });
+});
+
+function checkExistingIds(inputValue) {
+  const elementsWithIds = Array.from(document.querySelectorAll("[id]"));
+  const idsArray = elementsWithIds.map((element) => element.id);
+  console.log("IDs array:", idsArray);
+  let usersId = idsArray.find((ele) => ele == inputValue);
+  return usersId;
+}
+
+function displayMessage() {
+  const usersId = checkExistingIds();
+  if (usersId) {
+    console.log("id already exists");
+    const existsMessage = document.getElementById("exists");
+    existsMessage.style.display = "block";
+  } else {
+    console.log("id is avaliable");
+    const avaliableMessage = document.getElementById("avaliable");
+    avaliableMessage.style.display = "block";
+  }
+}
