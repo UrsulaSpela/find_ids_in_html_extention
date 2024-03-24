4f1723ce085d1a3be665e73322b172f29e0c7a9d
Ursula Kralj <ursula.spela@gmail.com>
2024-03-24 17:11:11 +0100

    remove useless code

--- contentScript.js
+++ contentScript.js
@@ -1,14 +1,1 @@
-// contentScript.js
 
-function getAllIds() {
-  const elementsWithIds = Array.from(document.querySelectorAll('[id]'));
-  const idsArray = elementsWithIds.map(element => element.id);
-  return idsArray;
-}
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "getIds") {
-    const ids = getAllIds();
-    sendResponse(ids);
-  }
-});
